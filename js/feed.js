f2912c702beeb7b97344bdcb3f47ec242e0aa582
cecsun <ceciliesunde94@gmail.com>
2023-12-04 19:00:15 +0100

    Posts

--- js/feed.js
+++ js/feed.js
@@ -1,6 +1,19 @@
 const API_BASE_URL = 'https://api.noroff.dev';
 
+// const json = response.json();
+// console.log(json);
+
 const viewPosts = document.querySelector(".feed-posts");
+
+
+// json.forEach((postData) => {
+//     let postElement = postTemplate.cloneNode(true);
+
+//     postElement.querySelector(".form-label").innerText = postData.title;
+//     postElement.querySelector(".img-thumbnail").innerText = postData.media;
+
+//     viewPosts.appendChild(postElement);
+// });
 
 async function fetchWithToken(url) {
     try {
@@ -17,20 +30,36 @@ async function fetchWithToken(url) {
         console.log(response);
         const json = await response.json();
         console.log(json);
+  
 
-        const postTemplate = document.querySelector(".post-template");
-        let post = postTemplate.cloneNode(true);
-        post.(".form-label")
+        for (let i = 0; i < json.length; i++) {
+            let element = json[i];
+            let media = element.media;
+            if (media == null || media == "") {
+                continue;
+            }
+            let title = element.title;
+            const postTemplate = document.querySelector(".post-template");
+            let post = postTemplate.cloneNode(true);
+            let img = post.querySelector("img");
+            img.src = media;
+            let username = post.querySelector("label");
+            username.innerHTML = title;
+            // post.style.display = "unset";
+            viewPosts.appendChild(post);
+        }
+
+    
+
+        
 
 
+        // const postMedia = post.querySelector(".img-thumbnail");
+        // let media = postMedia.cloneNode(true);
+
+
+        
         viewPosts.appendChild(post);
-
-        // let pathLike = document.createElement('path');
-        // pathLike.d = "M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z";
-        // let svgLike = document.createElement('svg');
-        // svgLike.xmlns = "http://www.w3.org/2000/svg";
-
-
 
     } catch (error) {
         console.log(error);
